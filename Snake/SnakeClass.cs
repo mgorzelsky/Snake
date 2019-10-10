171ce0be99eb832c2c8b93353da5ca5e13190f3c
greymiba <greymiba@gmail.com>
2019-10-10 15:20:23 -0700

    name change: Program.cs --> SnakeClass.cs

--- Snake/SnakeClass.cs
+++ Snake/SnakeClass.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 
 namespace Snake
 {
-    class Snake
+    class SnakeClass
     {
 
         private int xPos;
         private int yPos;
         private List<(int, int)> body;
+        private (int x, int y) headPos;
+        private (int x, int y) tailPos;
 
         //static void Main(string[] args)
         //{
-        public Snake()
+        public SnakeClass()
         {
-            body = new List<(int, int)>(3);
+            headPos = (5, 5); // Arbitrary head & tail initial position.
+            tailPos = (3, 5);
+            body = new List<(int, int)>(3) { (headPos), (headPos.x - 1, tailPos.y), (tailPos) };
         }
 
         public static void Eat()
