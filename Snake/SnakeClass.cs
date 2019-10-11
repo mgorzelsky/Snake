279683c804d56400e2bacd638e28f27e81b3d09e
greymiba <greymiba@gmail.com>
2019-10-10 22:35:47 -0700

    Implemented methods: Move, GetSnakePosition, ChangeDirection. Methods NOT yet implemented: Lengthen, Eat. Still needs Unit Testing.

--- Snake/SnakeClass.cs
+++ Snake/SnakeClass.cs
@@ -4,17 +4,16 @@ using System.Drawing;
 
 namespace Snake
 {
+    enum direction { right = 1, up = 2, left = 3, down = 4 };
+
     class SnakeClass
     {
 
-        //private int xPos;
-        //private int yPos;
         private static List<Point> body;
         private static Point headPos;
         private static Point tailPos;
+        private static int currentDirection = direction.right;
 
-        //static void Main(string[] args)
-        //{
         public SnakeClass()
         {
             headPos.X = tailPos.X = 5; // Arbitrary head & tail initial position.
@@ -36,22 +35,67 @@ namespace Snake
 
         }
 
-        public static void GetDirection()
+        // Returns string "right", "up", "left", or "down" indicating the direction of head travel
+        public static string GetDirection()
         {
-
+            return (Enum.GetName(currentDirection));
         }
 
-        // Index 1 is the snake head
+        // ChangeDirection takes the string "right", "up", "left", or "down" indicating the new direction of the snake. The string input is related to an int
+        // per the enum above. 
+        public static void ChangeDirection(string vector)
+        {
+            if (vector == Enum.GetName(1)) currentDirection = direction.right;
+            else if (vector == Enum.GetName(2)) { currentDirection = direction.up; }
+            else if (vector == Enum.GetName(3)) { currentDirection = direction.left; }
+            else if (vector == Enum.GetName(4)) { currentDirection = direction.down; }
+            
+        }
+
+        // This method returns a List of Points containing snake body coordinates. **Index 0 is the snake head.**
         public static List<Point> GetSnakePosition()
         {
             return (body);
         }
 
+        // Move(): Store current headPos, get current direction & change Point coordinate of headPos accordingly. The second unit of snake will then assume 
+        // the previous Point coordinate of the headPos, the third unit of snake will then assume the previous position of the second unit, and so on, until
+        // all parts of the snake have advanced. 
         public static void Move()
         {
+            Point oldPosition1 = headPos;
 
+            switch ((direction)currentDirection)
+            {
+                case ("right"):
+                    {
+                        headPos.X += 1;
+                        break;
+                    }
+                case ("up"):
+                    {
+                        headPos.Y -= 1; // Up is in the -Y direction on the console axis.
+                        break;
+                    }
+                case ("left"):
+                    {
+                        headPos.X -= 1;
+                        break;
+                    }
+                case ("down"):
+                    {
+                        headPos.Y += 1; // Down is in the +Y direction on the console axis.
+                        break;
+                    }
+
+            }
+
+            for(int index = 1; index < body.Count; index++)
+            {
+                Point oldPosition2 = body[index];
+                body[index] = oldPosition1;
+                oldPosition1 = oldPosition2;
+            }
         }
-
-        //}
     }
 }
