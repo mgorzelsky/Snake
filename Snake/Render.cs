aaf98013062120b02e4a51efd5efaa5be1e09648
Mike Gorzelsky <m.d.gorzelsky@gmail.com>
2019-10-11 11:22:15 -0700

    Added render class as workaround from failed pull requests.

---
+++ Snake/Render.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class Render
+    {
+        //  variables
+
+        //  methods
+        public void DrawScreen(StateOfLocation[,] gameBoard)
+        {
+            for (int i = 0; i < gameBoard.GetLength(0); i++)
+            {
+                for (int j = 0; j < gameBoard.GetLength(1); j++)
+                {
+                    Console.SetCursorPosition(j, i);        // this method requires you send the columns first (x values), then rows
+                    switch (gameBoard[i, j])
+                    {
+                        case StateOfLocation.Empty:
+                            Console.Write("-");
+                            break;
+                        case StateOfLocation.Snake:
+                            Console.Write("S");
+                            break;
+                        case StateOfLocation.Food:
+                            Console.Write("F");
+                            break;
+                        default:
+                            break;
+                    }
+
+                }
+                Console.WriteLine();
+            }
+        }
+        //  constructors
+
+    }
+}
